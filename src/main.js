b5d10cbd9afc45d2e45304d82bb3805c407476d0
chhw130 <527coco@naver.com>
2023-10-01 17:31:52 +0900

    Feat : Vuex Store

--- src/main.js
+++ src/main.js
@@ -1,7 +1,18 @@
 import { createApp } from 'vue';
 
 import App from './App.vue';
+import { createStore } from 'vuex';
 
 const app = createApp(App);
+
+const store = createStore({
+  state() {
+    return {
+      counter: 0,
+    };
+  },
+});
+
+app.use(store);
 
 app.mount('#app');
